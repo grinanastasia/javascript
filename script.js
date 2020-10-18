f224268c89c70f6753ac340b05af4841d891130a
Nastya <grin2008.96@mail.ru>
2020-10-18 21:47:39 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/* "use strict";
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false,
+};
+console.log(persone["name"]);
+
+let arr = ["plump.png", "ot.png", "hhhd.png"];
+console.log(arr[0]); */
+
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5 % 2);
+console.log("2" == 2);
+console.log("2" === 2);
+
+/* let isChecked = true,
+  isClose = true;
+console.log(isChecked && isClose); /* оператор или */
+
+let isChecked = false,
+  isClose = false;
+console.log(isChecked || !isClose);
+console.log(incr++);
