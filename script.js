a7eebd8470fd1e7867a668a54eb648da6adafe1c
Nastya <grin2008.96@mail.ru>
2020-10-25 22:24:30 +0300

    add obj+mass

--- script.js
+++ script.js
@@ -63,7 +63,72 @@ console.log(
 ); /* .length это свойство, которое описывает переменную */
 
 console.log(str.toUpperCase()); /* здесь метод */
-let tvelve = 12.2px;
+let tvelv = 12.2;
 /* console.log(Math.round(tvelve)); */
-console.log(parseInt(tvelve)); /* возвращает целое число */
-console.log(parseFloat(tvelve)); /* возвращает дробное чило */
+console.log(parseInt(tvelv)); /* возвращает целое число */
+console.log(parseFloat(tvelv)); /* возвращает дробное чило */
+
+function learnJS(lang, callback) {
+  /* когда нужно чтобы функции шли одна за другой */
+  console.log("Я учу " + lang);
+  callback();
+}
+learnJS("JavaScript", function () {
+  console.log("Я прошел 3й урок");
+});
+
+let options = {
+  wight: 1024,
+  height: 1024,
+  name: "test",
+};
+console.log(options.name); /* выводит только свойство name */
+options.bool = false;
+delete options.bool;
+for (let key in options) {
+  console.log("Свойство " + key + " имеет значание" + options[key]);
+}
+console.log(Object.keys(options).length); /* выводит количество свойств */
+
+// arr[99] = 99;  добавляет 99 элемент в массив
+// console.log(arr.length);  в данном случае length возвращает последний индекс элемента +1 =100
+// arr.pop();   удаляет последний элемент массива
+// arr.push("5");  добавляет элемент к массиву
+// arr.shift();  убирает первый элемент массива
+// arr.unshift("1");  добавляет элемент в начало массива
+
+// for (let i = 0; i < arr.length; i++) {
+//   /* перебирает элементы массива */
+//   console.log(arr[i]);
+
+// let arr = ["first", 2, 3, "four", 5];
+// arr.forEach(function (item, i, mass) {
+//   /* перебрат все элементы массива */
+//   console.log(i + ": " + item + " (массив: " + mass + ")");
+// });
+// console.log(arr);
+
+// let mass = [1, 3, 4, 5, 7];
+// for (let key of mass) {
+//   /* при помощи for/in получаем ключи массива, а не сам массив, если in заменить на of, то получим значения*/
+//   console.log(key);
+// }
+
+// let ans = prompt("", ""),
+//   arr = [];
+// arr = ans.split(","); /* то что ввели на сайте отобразилось в массив */
+// console.log(arr);
+
+// let arr = ["klajflka", "gdggd", "shhsh"],
+//   i = arr.join(", "); /* склеивает все в одну стороку */
+// console.log(i);
+
+// let arr = ["klajflka", "gdggd", "shhsh"],
+//   i = arr.sort(); /* отсортировать по алфафиту */
+// console.log(arr);
+let arr = [1, 5, 13, 6],
+  i = arr.sort(compaerNum); /* отсортировать по порядку числа */
+function compaerNum(a, b) {
+  return a - b;
+}
+console.log(arr);
